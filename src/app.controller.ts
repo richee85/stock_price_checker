55dbe70575b7064e809a0618c4437d1be6c1df42
Richard Galambos <richee85@gmail.com>
2024-03-18 20:30:43 +0100

    fix db connection

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Put } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/stock/:symbol')
   getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Put('/stock/:symbol')
+  getWellHello(): string {
     return this.appService.getHello();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'user',
+      password: 'almafa123',
+      database: 'stock_prices',
+      entities: [],
+      subscribers: [],
+      migrations: [],
+      logging: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
